a33eb627aa69ac8c833dfb53277edae1ccd1f7a5
JakubPelc19 <jakubpelc86@gmail.com>
2025-11-06 15:50:33 +0100

    CopyButton was created, ResultComponent first design done

--- app/ResultComponent.tsx
+++ app/ResultComponent.tsx
@@ -1,8 +1,12 @@
+import CopyButton from "./CopyButton";
+
 export default function ResultComponent() {
 
     return (
-    <div className="bg-[#e6e6e6] w-full h-14 mt-2 rounded-2xl flex pl-4 pr-2">
-
+    <div className="bg-[#e6e6e6] justify-between items-center w-full h-14 mt-2 rounded-2xl flex pl-4 pr-2">
+        <p>http://example.com/uilki8</p>
+        
+        <CopyButton/>
     </div>
 )
 }
