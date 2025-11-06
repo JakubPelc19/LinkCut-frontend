561ad1c26962e66d33034e21de93eb00a600d4e9
JakubPelc19 <jakubpelc86@gmail.com>
2025-11-06 16:14:39 +0100

    url form action created

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,21 @@ import ResultComponent from "./ResultComponent";
 import SendButton from "./SendButton";
 
 
+
 export default function Home() {
+  
+  async function urlFormAction(formData: FormData) {
+    'use server'
+
+    const url = formData.get("urlInput")?.toString();
+
+
+  }
+
   return (
     <main className="w-[600px] h-56 flex flex-col mb-10 items-center">
       <h1 className="text-3xl mb-10">LinkCut</h1>
-        <form className="bg-[#e6e6e6] w-full h-14 rounded-2xl flex pl-4 pr-2">
+        <form action={urlFormAction} className="bg-[#e6e6e6] w-full h-14 rounded-2xl flex pl-4 pr-2">
           <input type="text" className="w-[575px] h-14 outline-0" name="urlInput" id="urlInput" placeholder="Link" required/>
           <SendButton/>
         </form>
