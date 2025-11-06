f48cad8ed579bc66ad8aaf928b0b44cd55e44bd2
JakubPelc19 <jakubpelc86@gmail.com>
2025-11-06 14:55:34 +0100

    basic structure of HomePage

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="w-screen h-screen m-0 p-0">
+      <body className="w-screen h-screen m-0 p-0 flex justify-center items-center">
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@
 
 export default function Home() {
   return (
-    <></>
+    <main className="w-[40%]">
+      <h1>LinkCut</h1>
+      <form>
+        <input type="text" name="urlInput" id="urlInput" required/>
+        <button type="submit"></button>
+      </form>
+    </main>
   );
 }
