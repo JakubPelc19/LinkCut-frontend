dd1aec6d9c22d94335610feed4c43bfa51765073
JakubPelc19 <jakubpelc86@gmail.com>
2025-11-06 15:07:54 +0100

    Change of desing in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 export default function Home() {
   return (
-    <main className="w-[40%]">
-      <h1>LinkCut</h1>
-      <form>
-        <input type="text" name="urlInput" id="urlInput" required/>
-        <button type="submit"></button>
-      </form>
+    <main className="w-[600px] h-44 flex flex-col mb-10 items-center">
+      <h1 className="text-3xl mb-10">LinkCut</h1>
+      <div className="bg-[#e1e1e1] w-full h-14">
+        <form>
+          <input type="text" className="w-[575px]" name="urlInput" id="urlInput" required/>
+          <button type="submit"></button>
+        </form>
+      </div>
     </main>
   );
 }
